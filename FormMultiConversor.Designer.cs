641379eae911b6ff95e7b3494e1dfa9c70088b1d
Ítalo Penha <101754876+italopenha@users.noreply.github.com>
2025-08-05 20:26:28 -0300

    Commit inicial

---
+++ FormMultiConversor.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace MultiConversor
+{
+    partial class FormMultiConversor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbFahrenheit = new System.Windows.Forms.GroupBox();
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.gbCelsius = new System.Windows.Forms.GroupBox();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.gbTemperatura = new System.Windows.Forms.GroupBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.gbMoeda = new System.Windows.Forms.GroupBox();
+            this.gbReal = new System.Windows.Forms.GroupBox();
+            this.txtReal = new System.Windows.Forms.TextBox();
+            this.gbDolar = new System.Windows.Forms.GroupBox();
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.gbCotacaoDolar = new System.Windows.Forms.GroupBox();
+            this.txtCotacaoDolar = new System.Windows.Forms.TextBox();
+            this.gbFahrenheit.SuspendLayout();
+            this.gbCelsius.SuspendLayout();
+            this.gbTemperatura.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.gbMoeda.SuspendLayout();
+            this.gbReal.SuspendLayout();
+            this.gbDolar.SuspendLayout();
+            this.gbCotacaoDolar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbFahrenheit
+            // 
+            this.gbFahrenheit.Controls.Add(this.txtFahrenheit);
+            this.gbFahrenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbFahrenheit.Location = new System.Drawing.Point(6, 25);
+            this.gbFahrenheit.Name = "gbFahrenheit";
+            this.gbFahrenheit.Size = new System.Drawing.Size(137, 61);
+            this.gbFahrenheit.TabIndex = 0;
+            this.gbFahrenheit.TabStop = false;
+            this.gbFahrenheit.Text = "Fahrenheit - °F";
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Location = new System.Drawing.Point(6, 25);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(125, 26);
+            this.txtFahrenheit.TabIndex = 2;
+            this.txtFahrenheit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFahrenheit_KeyPress);
+            this.txtFahrenheit.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFahrenheit_KeyUp);
+            // 
+            // gbCelsius
+            // 
+            this.gbCelsius.Controls.Add(this.txtCelsius);
+            this.gbCelsius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbCelsius.Location = new System.Drawing.Point(6, 92);
+            this.gbCelsius.Name = "gbCelsius";
+            this.gbCelsius.Size = new System.Drawing.Size(137, 61);
+            this.gbCelsius.TabIndex = 1;
+            this.gbCelsius.TabStop = false;
+            this.gbCelsius.Text = "Celsius - °C";
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(7, 25);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(124, 26);
+            this.txtCelsius.TabIndex = 3;
+            this.txtCelsius.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCelsius_KeyPress);
+            this.txtCelsius.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtCelsius_KeyUp);
+            // 
+            // gbTemperatura
+            // 
+            this.gbTemperatura.Controls.Add(this.gbCelsius);
+            this.gbTemperatura.Controls.Add(this.gbFahrenheit);
+            this.gbTemperatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbTemperatura.Location = new System.Drawing.Point(12, 12);
+            this.gbTemperatura.Name = "gbTemperatura";
+            this.gbTemperatura.Size = new System.Drawing.Size(154, 161);
+            this.gbTemperatura.TabIndex = 2;
+            this.gbTemperatura.TabStop = false;
+            this.gbTemperatura.Text = "Temperatura";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // gbMoeda
+            // 
+            this.gbMoeda.Controls.Add(this.gbCotacaoDolar);
+            this.gbMoeda.Controls.Add(this.gbReal);
+            this.gbMoeda.Controls.Add(this.gbDolar);
+            this.gbMoeda.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbMoeda.Location = new System.Drawing.Point(198, 12);
+            this.gbMoeda.Name = "gbMoeda";
+            this.gbMoeda.Size = new System.Drawing.Size(178, 228);
+            this.gbMoeda.TabIndex = 3;
+            this.gbMoeda.TabStop = false;
+            this.gbMoeda.Text = "Moeda";
+            // 
+            // gbReal
+            // 
+            this.gbReal.Controls.Add(this.txtReal);
+            this.gbReal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbReal.Location = new System.Drawing.Point(6, 159);
+            this.gbReal.Name = "gbReal";
+            this.gbReal.Size = new System.Drawing.Size(137, 61);
+            this.gbReal.TabIndex = 1;
+            this.gbReal.TabStop = false;
+            this.gbReal.Text = "Real - R$";
+            // 
+            // txtReal
+            // 
+            this.txtReal.Location = new System.Drawing.Point(7, 25);
+            this.txtReal.Name = "txtReal";
+            this.txtReal.Size = new System.Drawing.Size(124, 26);
+            this.txtReal.TabIndex = 3;
+            this.txtReal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtReal_KeyPress);
+            this.txtReal.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtReal_KeyUp);
+            // 
+            // gbDolar
+            // 
+            this.gbDolar.Controls.Add(this.txtDolar);
+            this.gbDolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbDolar.Location = new System.Drawing.Point(6, 92);
+            this.gbDolar.Name = "gbDolar";
+            this.gbDolar.Size = new System.Drawing.Size(137, 61);
+            this.gbDolar.TabIndex = 0;
+            this.gbDolar.TabStop = false;
+            this.gbDolar.Text = "Dólar - US$";
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(6, 25);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(125, 26);
+            this.txtDolar.TabIndex = 2;
+            this.txtDolar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDolar_KeyPress);
+            this.txtDolar.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtDolar_KeyUp);
+            // 
+            // gbCotacaoDolar
+            // 
+            this.gbCotacaoDolar.Controls.Add(this.txtCotacaoDolar);
+            this.gbCotacaoDolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbCotacaoDolar.Location = new System.Drawing.Point(6, 25);
+            this.gbCotacaoDolar.Name = "gbCotacaoDolar";
+            this.gbCotacaoDolar.Size = new System.Drawing.Size(162, 61);
+            this.gbCotacaoDolar.TabIndex = 4;
+            this.gbCotacaoDolar.TabStop = false;
+            this.gbCotacaoDolar.Text = "Cotação Dólar Hoje";
+            // 
+            // txtCotacaoDolar
+            // 
+            this.txtCotacaoDolar.Location = new System.Drawing.Point(6, 25);
+            this.txtCotacaoDolar.Name = "txtCotacaoDolar";
+            this.txtCotacaoDolar.Size = new System.Drawing.Size(125, 26);
+            this.txtCotacaoDolar.TabIndex = 2;
+            // 
+            // FormMultiConversor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 639);
+            this.Controls.Add(this.gbMoeda);
+            this.Controls.Add(this.gbTemperatura);
+            this.Name = "FormMultiConversor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MultiConversor";
+            this.Load += new System.EventHandler(this.FormMultiConversor_Load);
+            this.gbFahrenheit.ResumeLayout(false);
+            this.gbFahrenheit.PerformLayout();
+            this.gbCelsius.ResumeLayout(false);
+            this.gbCelsius.PerformLayout();
+            this.gbTemperatura.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.gbMoeda.ResumeLayout(false);
+            this.gbReal.ResumeLayout(false);
+            this.gbReal.PerformLayout();
+            this.gbDolar.ResumeLayout(false);
+            this.gbDolar.PerformLayout();
+            this.gbCotacaoDolar.ResumeLayout(false);
+            this.gbCotacaoDolar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbFahrenheit;
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.GroupBox gbCelsius;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.GroupBox gbTemperatura;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.GroupBox gbMoeda;
+        private System.Windows.Forms.GroupBox gbReal;
+        private System.Windows.Forms.TextBox txtReal;
+        private System.Windows.Forms.GroupBox gbDolar;
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.GroupBox gbCotacaoDolar;
+        private System.Windows.Forms.TextBox txtCotacaoDolar;
+    }
+}
---
+++ FormMultiConversor.cs
@@ -0,0 +1,453 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiConversor
+{
+    public partial class FormMultiConversor : Form
+    {
+        public FormMultiConversor()
+        {
+            InitializeComponent();
+            txtCotacaoDolar.Enabled = false;
+        }
+
+        #region Conversão de Temperaturas
+
+        /// <summary>
+        /// Método para converter Fahrenheit para Celsius.
+        /// </summary>
+        private void ConverterFahrenheitParaCelsius()
+        {
+            try
+            {
+                txtCelsius.Text = string.Empty; // Limpa o campo de saída da antes de converter
+
+                // Verifica se o texto digitado é um número válido
+                if (!double.TryParse(txtFahrenheit.Text, out double fahrenheit))
+                    return;
+
+                double celsius = (fahrenheit - 32) / 1.8f;
+                txtCelsius.Text = celsius.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Método para converter Celsius para Fahrenheit.
+        /// </summary>
+        private void ConverterCelsiusParaFahrenheit()
+        {
+            try
+            {
+                txtFahrenheit.Text = string.Empty; // Limpa o campo de saída da antes de converter
+
+                if (!double.TryParse(txtCelsius.Text, out double celsius))
+                    return;
+
+                double fahrenheit = (celsius * 1.8f) + 32;
+                txtFahrenheit.Text = fahrenheit.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void txtFahrenheit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                TextBox textBox = sender as TextBox;
+
+                // Permite teclas de controle (ex: backspace, delete)
+                if (char.IsControl(e.KeyChar))
+                {
+                    return;
+                }
+
+                // Permite dígitos (0-9)
+                if (char.IsDigit(e.KeyChar))
+                {
+                    return;
+                }
+
+                // Permite vírgula, mas apenas se:
+                // - ainda não houver uma vírgula
+                // - já houver ao menos um dígito antes dela
+                if (e.KeyChar == ',')
+                {
+                    // Já tem vírgula? Ou não tem número ainda? Então bloqueia
+                    if (textBox.Text.Contains(',') || textBox.Text.Length == 0 || textBox.SelectionStart == 0)
+                    {
+                        e.Handled = true;
+                    }
+
+                    return;
+                }
+
+                // Permite sinal de menos, mas apenas se:
+                // - ele estiver no início
+                // - ainda não houver um sinal de menos
+                if (e.KeyChar == '-')
+                {
+                    if (textBox.SelectionStart != 0 || textBox.Text.Contains('-'))
+                    {
+                        e.Handled = true;
+                    }
+
+                    return;
+                }
+
+                // Bloqueia qualquer outra tecla
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void txtFahrenheit_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(txtFahrenheit.Text) && txtFahrenheit.Text != "-") // Verifica se o campo não está vazio  
+                {
+                    ConverterFahrenheitParaCelsius();
+                }
+                else
+                {
+                    txtCelsius.Text = string.Empty; // Limpa o campo de saída se o campo de entrada estiver vazio  
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void txtCelsius_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                TextBox textBox = sender as TextBox;
+
+                // Permite teclas de controle (ex: backspace, delete)
+                if (char.IsControl(e.KeyChar))
+                {
+                    return;
+                }
+
+                // Permite dígitos (0-9)
+                if (char.IsDigit(e.KeyChar))
+                {
+                    return;
+                }
+
+                // Permite vírgula, mas apenas se:
+                // - ainda não houver uma vírgula
+                // - já houver ao menos um dígito antes dela
+                if (e.KeyChar == ',')
+                {
+                    // Já tem vírgula? Ou não tem número ainda? Então bloqueia
+                    if (textBox.Text.Contains(',') || textBox.Text.Length == 0 || textBox.SelectionStart == 0)
+                    {
+                        e.Handled = true;
+                    }
+
+                    return;
+                }
+
+                // Permite sinal de menos, mas apenas se:
+                // - ele estiver no início
+                // - ainda não houver um sinal de menos
+                if (e.KeyChar == '-')
+                {
+                    if (textBox.SelectionStart != 0 || textBox.Text.Contains('-'))
+                    {
+                        e.Handled = true;
+                    }
+
+                    return;
+                }
+
+                // Bloqueia qualquer outra tecla
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void txtCelsius_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(txtCelsius.Text) && txtCelsius.Text != "-") // Verifica se o campo não está vazio  
+                {
+                    ConverterCelsiusParaFahrenheit();
+                }
+                else
+                {
+                    txtFahrenheit.Text = string.Empty; // Limpa o campo de saída se o campo de entrada estiver vazio  
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Conversão de Moedas
+
+        /// <summary>
+        /// Método para obter a cotação de uma moeda em relação a outra usando a API ExchangeRate-API.
+        /// </summary>
+        /// <param name="moedaOrigem"></param>
+        /// <param name="moedaDestino"></param>
+        /// <returns></returns>
+        private async Task<decimal> ObterCotacaoAsync(string moedaOrigem, string moedaDestino)
+        {
+            try
+            {
+                using (HttpClient cliente = new HttpClient())
+                {
+                    HttpResponseMessage resposta = await cliente.GetAsync($"https://v6.exchangerate-api.com/v6/e49bcf089726ccc1cbfb750b/latest/{moedaOrigem}");
+                    if (resposta.IsSuccessStatusCode)
+                    {
+                        string jsonString = await resposta.Content.ReadAsStringAsync();
+                        var dados = JsonConvert.DeserializeObject<RespostaTaxaCambio>(jsonString);
+                        return dados.conversion_rates[moedaDestino];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro ao obter a cotação");
+                        return 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Método para converter Dólar para Real.
+        /// </summary>
+        private async void ConverterDolarParaRealAsync()
+        {
+            try
+            {
+                txtReal.Text = string.Empty; // Limpa o campo de saída da antes de converter
+
+                if (!decimal.TryParse(txtDolar.Text, out decimal dolar))
+                    return;
+
+                decimal cotacaoDolar = await ObterCotacaoAsync("USD", "BRL");
+                decimal real = dolar * cotacaoDolar;
+                txtReal.Text = real.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Método para converter Real para Dólar.
+        /// </summary>
+        private async void ConverterRealParaDolarAsync()
+        {
+            try
+            {
+                txtDolar.Text = string.Empty; // Limpa o campo de saída da antes de converter
+
+                if (!decimal.TryParse(txtReal.Text, out decimal real))
+                    return;
+
+                decimal cotacaoReal = await ObterCotacaoAsync("BRL", "USD");
+                decimal dolar = real * cotacaoReal;
+                txtDolar.Text = dolar.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            } 
+        }
+
+        /// <summary>
+        /// Método para obter a cotação do Dólar ao iniciar o formulário.
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        private async void ObterCotacaoDolarAsync()
+        {
+            try
+            {
+                decimal cotacaoDolar = await ObterCotacaoAsync("USD", "BRL");
+                txtCotacaoDolar.Text = cotacaoDolar.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void txtDolar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                TextBox textBox = sender as TextBox;
+
+                // Permite teclas de controle (ex: backspace, delete)
+                if (char.IsControl(e.KeyChar))
+                {
+                    return;
+                }
+
+                // Permite dígitos (0-9)
+                if (char.IsDigit(e.KeyChar))
+                {
+                    return;
+                }
+
+                // Permite vírgula, mas apenas se:
+                // - ainda não houver uma vírgula
+                // - já houver ao menos um dígito antes dela
+                if (e.KeyChar == ',')
+                {
+                    // Já tem vírgula? Ou não tem número ainda? Então bloqueia
+                    if (textBox.Text.Contains(',') || textBox.Text.Length == 0 || textBox.SelectionStart == 0)
+                    {
+                        e.Handled = true;
+                    }
+
+                    return;
+                }
+
+                // Bloqueia qualquer outra tecla
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void txtDolar_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(txtDolar.Text)) // Verifica se o campo não está vazio  
+                {
+                    ConverterDolarParaRealAsync();
+                }
+                else
+                {
+                    txtReal.Text = string.Empty; // Limpa o campo de saída se o campo de entrada estiver vazio  
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void txtReal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                TextBox textBox = sender as TextBox;
+
+                // Permite teclas de controle (ex: backspace, delete)
+                if (char.IsControl(e.KeyChar))
+                {
+                    return;
+                }
+
+                // Permite dígitos (0-9)
+                if (char.IsDigit(e.KeyChar))
+                {
+                    return;
+                }
+
+                // Permite vírgula, mas apenas se:
+                // - ainda não houver uma vírgula
+                // - já houver ao menos um dígito antes dela
+                if (e.KeyChar == ',')
+                {
+                    // Já tem vírgula? Ou não tem número ainda? Então bloqueia
+                    if (textBox.Text.Contains(',') || textBox.Text.Length == 0 || textBox.SelectionStart == 0)
+                    {
+                        e.Handled = true;
+                    }
+
+                    return;
+                }
+
+                // Bloqueia qualquer outra tecla
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void txtReal_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(txtReal.Text)) // Verifica se o campo não está vazio  
+                {
+                    ConverterRealParaDolarAsync();
+                }
+                else
+                {
+                    txtDolar.Text = string.Empty; // Limpa o campo de saída se o campo de entrada estiver vazio  
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void FormMultiConversor_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ObterCotacaoDolarAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Classe para armazenar a resposta da API de taxa de câmbio.
+    /// </summary>
+    public class RespostaTaxaCambio
+    {
+        public Dictionary<string, decimal> conversion_rates { get; set; }
+    }
+}
